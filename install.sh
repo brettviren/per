e1c0d989879816cf43cfc35c6f89ff23b59d100e
Brett Viren <brett.viren@gmail.com>
2024-09-07 14:28:49 -0400

    Add titome, fixes

--- install.sh
+++ install.sh
@@ -10,24 +10,30 @@ if [ -z "$VIRTUAL_ENV" ] ; then
 fi
 
 set -e
-set -x
 
-
-pip install pyinstaller
+pip install pyinstaller > $perdir/logs/pip-install.log 2>&1
 
 do_install () {
     main=$1 ; shift
+    echo "installing $main" 
     
+    logd=$perdir/logs/$main
+    if [ ! -d $logd ] ; then
+        mkdir -p $logd
+    fi
+
+    # rm -rf build dist ${main}.spec
+
     if [ -f $main/requirements.txt ] ; then
         cd $main
-        pip install -r requirements.txt
-        cd -
+        pip install -r requirements.txt > $logd/pip-install.log 2>&1
+        cd ..
     fi
 
     args="--onefile -n $main"
     for mod in $@
     do
-        args="$args --hidden-import $mod"
+        args="$args --hidden-imports $mod"
     done
 
     mainsrc=""
@@ -39,9 +45,12 @@ do_install () {
         echo "SKIPPING package $main, does not fit any pattern."
         return
     fi
-    pyinstaller $args $mainsrc
+    # echo pyinstaller $args $mainsrc
+    pyinstaller $args $mainsrc >> $logd/pyinstaller.log 2>&1
 
     if [ -f dist/$main ] ; then
+        # remove first to avoid "Text file is busy"
+        rm -f ~/sync/bin/$main
         cp dist/$main ~/sync/bin/
     else
         echo "FAILED to build $main"
@@ -51,4 +60,4 @@ do_install () {
 do_install rephile
 do_install herbie
 do_install barpyrus multiprocessing
-
+do_install titome 
